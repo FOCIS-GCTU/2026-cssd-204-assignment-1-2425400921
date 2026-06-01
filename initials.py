fe2338a32388d342441c4058976e00f609efe8b9
2425400921 <2425400921@live.gctu.edu.gh>
2026-06-01 10:05:06 +0000

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,37 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: OSCAR DEWODO KWASHIE
+# STUDENT ID: 2425400921
+# Email: OSCARDEWODOKWASHIE@GMAIL.COM
+# Grader: MR. AUGUSTUS BUCKMAN
+#
+# On my honor, OSCAR DEWODO KWASHIE, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+def main():
+    # Print my initials in large letters
+
+    print()
+    print("...ODK")
+    print()
+
+    print("...OOOOOOOOOO........DDDDDDDDDD..........KK......KK.....")
+    print("...OO......OO........DD.......DD.........KK.....KK......")
+    print("...OO......OO........DD........DD........KK....KK.......")
+    print("...OO......OO........DD........DD........KK...KK........")
+    print("...OO......OO........DD........DD........KK..KK.........")
+    print("...OO......OO........DD........DD........KK.KK..........")
+    print("...OO......OO........DD........DD........KK..KK.........")
+    print("...OO......OO........DD.......DD.........KK....KK.......")
+    print("...OO......OO...**...DD......DD.....**...KK.....KK....**")
+    print("...0000000000...**...DDDDDDDDD......**...KK......KK...**")
+
+    print()
+
+
+
+main()
